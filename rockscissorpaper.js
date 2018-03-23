69f561950efa93daf88e2f7f6e1996741cd810b6
Ida <ida.bergstrom@medieinstitutet.se>
2018-03-23 15:19:31 +0100

    Successful day here at MI

---
+++ rockscissorpaper.js
@@ -0,0 +1,62 @@
+var personAPoints = 0;
+var personBPoints = 0;
+
+function playRockScissorPaper(input){
+    let arrayOfInput = splitString(input);
+    
+    for(let i = 0; i < arrayOfInput.length; i=i+2){
+        
+        let personA = arrayOfInput[i];
+        let personB = arrayOfInput[i+1];
+        
+        if(personA == 'R' && personB == 'S'){
+            personAPoints++;
+            console.log("Person A wins");
+        }else if(personA == 'S' && personB == 'S'){
+            console.log("Draw!");     
+        }else if(personA == 'P' && personB == 'S'){
+            personBPoints++;
+            console.log("Person B wins");
+        }else if(personA == 'R' && personB == 'R'){
+            console.log("Draw!");     
+        }else if(personA == 'P' && personB == 'P'){
+            console.log("Draw!");     
+        }else if(personB == 'R' && personA == 'S'){
+            personBPoints++;
+            console.log("Person B wins");
+        }else if(personB == 'P' && personA == 'S'){
+            personAPoints++;
+            console.log("Person A wins");
+        }  
+    }
+    whoWinsAll();   
+}
+
+function whoWinsAll(){
+    if(personAPoints > personBPoints){
+        console.log("Person A is the winner!!!! WOOOHOO!");
+    }else if(personBPoints > personAPoints){
+        console.log("Person B is the best! Congratulations!");
+    }else{
+        console.log("It's a draw today. You are both winners and/or losers. Have a good day y'all.");
+    }
+}
+
+function splitString(string){
+    let result = string.split('');
+    return result;
+}
+
+
+
+
+
+
+
+
+playRockScissorPaper("PRSRSPPRSSSPR");
+
+
+
+
+
